afb84c67f6caf7994526f67f065be8bc15a73d5e
javierreraul <758906@unizar.es>
2020-05-17 17:49:33 +0200

    Añadidos ejemplos de ejecución para todos los ejercicios. Corregida errata en 5_emit_log_topic.py

--- 5_emit_log_topic.py
+++ 5_emit_log_topic.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
-# python3.8 emit_log_topic.py "kern.critical" "A critical kernel error"
+# python3.8 5_emit_log_topic.py "kern.critical" "A critical kernel error"
 routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
 channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
